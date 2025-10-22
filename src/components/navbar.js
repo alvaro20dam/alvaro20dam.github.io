d9ad2a9e7e3926e50ce29554bc9910b7f5088fb1
Alvaro Gonzalez <alvaro20dam@hotmail.com>
2025-10-22 09:08:53 -0400

    Add some global styles to the page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,60 +1,19 @@
+// src/components/navbar.js
+
 import React from "react"
 import { Link } from "gatsby"
+import * as styles from "./navbar.module.css" // Import the new module
 
 export default function Navbar() {
   return (
     <nav>
-      <div className="links">
-        <Link
-          to="/projects"
-          style={{
-            fontSize: `var(--font-sm)`,
-            textDecoration: `none`,
-            marginLeft: `20px`,
-          }}
-        >
-          Portfolio Projects
-        </Link>
-        <Link
-          to="/about"
-          style={{
-            fontSize: `var(--font-sm)`,
-            textDecoration: `none`,
-            marginLeft: `20px`,
-          }}
-        >
-          About
-        </Link>
-        <Link
-          to="/page-2"
-          style={{
-            fontSize: `var(--font-sm)`,
-            textDecoration: `none`,
-            marginLeft: `20px`,
-          }}
-        >
-          Page-2
-        </Link>
-        <Link
-          to="/using-typescript"
-          style={{
-            fontSize: `var(--font-sm)`,
-            textDecoration: `none`,
-            marginLeft: `20px`,
-          }}
-        >
-          Page-TS
-        </Link>
-        <Link
-          to="/using-ssr"
-          style={{
-            fontSize: `var(--font-sm)`,
-            textDecoration: `none`,
-            marginLeft: `20px`,
-          }}
-        >
-          ssr
-        </Link>
+      {/* Use the imported class name */}
+      <div className={styles.links}>
+        <Link to="/projects">Portfolio Projects</Link>
+        <Link to="/about">About</Link>
+        <Link to="/page-2">Page-2</Link>
+        <Link to="/using-typescript">Page-TS</Link>
+        <Link to="/using-ssr">ssr</Link>
       </div>
     </nav>
   )
